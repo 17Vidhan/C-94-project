cc9f156227ee156991981d7aef4404041e4726ea
17Vidhan <88958533+17Vidhan@users.noreply.github.com>
2022-07-10 12:52:13 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,64 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCVgq6bl6EvLbBRbLgU5Q9E3Ld2mlx6ekA",
+    authDomain: "class-test-bb435.firebaseapp.com",
+    projectId: "class-test-bb435",
+    storageBucket: "class-test-bb435.appspot.com",
+    messagingSenderId: "430374127874",
+    appId: "1:430374127874:web:80aa2d7bd0b1484e409a1b"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  firebase.initializeApp(firebaseConfig);
+//ADD YOUR FIREBASE LINKS HERE
+
+user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "Welcome" + user_name + "!";
+
+function addRoom()
+{
+      room_name = document.getElementById("room_name").value;
+
+      firebase.database().ref("/").child(room_name).update({
+            purpose : "adding room name"
+      });
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+}
+
+
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = "";
+            snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+                  console.log("Room Name - " + Room_names);
+                  row = "<div class='room_name' id = " + Room_names + "onclick = 'redirectToRoomName(this.id)'>#" + Room_names + "</div> <hr>";
+                  document.getElementById("output").innerHTML += row
+
+                  //End code
+            });
+      });
+}
+getData();
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
+
+function logout()
+{
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+
+      window.location = "index.html";
+      
+}
